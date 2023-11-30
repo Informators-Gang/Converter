b0368f474020d8cffe4a1484b65c54870bcfbbe1
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2023-11-30 11:44:52 +0000

    Routing.

---
+++ file-conversion-service/internal/request_handlers/upload_handler.go
@@ -0,0 +1,10 @@
+package request_handlers
+
+import (
+	"net/http"
+	// Other necessary imports
+)
+
+func HandleFileUpload(w http.ResponseWriter, r *http.Request) {
+    // Logic to handle file uploading
+}
--- file-conversion-service/main.go
+++ file-conversion-service/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Informators-Gang/Converter/file-conversion-service/internal/request_handlers"
+)
+
+var port = flag.Int("port", 8000, "port to run the server on")
 
 func main() {
-	fmt.Println("hello world")
+    flag.Parse()
+
+    http.HandleFunc("/upload", request_handlers.HandleFileUpload)
+    http.HandleFunc("/convert", request_handlers.HandleConversionRequest)
+
+    addr := fmt.Sprintf(":%d", *port)
+    log.Printf("Starting server on %s\n", addr)
+    log.Fatal(http.ListenAndServe(addr, nil))
 }
