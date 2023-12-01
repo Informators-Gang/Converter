3781892d21e1d2e75823f8875f00c874c49d57dd
Vitalii Demediuk <demedyuk.v.i@gmail.com>
2023-12-01 00:06:19 +0000

    logs

--- file-conversion-service/internal/request_handlers/convert_handler.go
+++ file-conversion-service/internal/request_handlers/convert_handler.go
@@ -2,42 +2,45 @@ package request_handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/Informators-Gang/Converter/file-conversion-service/internal/custom_common"
 )
 
 type ConvertFileResponse struct {
-    Message    string `json:"message"`
     NewFileID  string `json:"new_file_id"`
 }
 
 func ConversionHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+    if r.Method != http.MethodPost {
+        log.Println("Error: Only POST method is allowed")
         http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
         return
     }
 
-	fileID := r.URL.Query().Get("file_id")
-	convertTo := r.URL.Query().Get("convert_to")
-	if fileID == "" || convertTo == "" {
-		http.Error(w, "File ID and conversion format are required", http.StatusBadRequest)
-		return
-	}
+    fileID := r.URL.Query().Get("file_id")
+    convertTo := r.URL.Query().Get("convert_to")
+    if fileID == "" || convertTo == "" {
+        log.Println("Error: File ID and conversion format are required")
+        http.Error(w, "File ID and conversion format are required", http.StatusBadRequest)
+        return
+    }
 
     newFileID, conversionErr := custom_common.ConvertFile(fileID, convertTo)
     if conversionErr != nil {
+        log.Println("Error converting file:", conversionErr)
         http.Error(w, conversionErr.Error(), http.StatusInternalServerError)
         return
     }
 
     response := ConvertFileResponse{
-        Message:   "File converted successfully",
         NewFileID: newFileID,
     }
 
     w.Header().Set("Content-Type", "application/json")
     if err := json.NewEncoder(w).Encode(response); err != nil {
+        log.Println("Error encoding JSON:", err)
         http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
     }
 }
